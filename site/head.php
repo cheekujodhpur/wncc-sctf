4ac652b555c9c5f9f5724b8891ac8111b6c5b6f9
Kumar Ayush <cheekujodhpur@gmail.com>
2015-12-27 06:16:24 -0500

    character encoding

--- site/head.php
+++ site/head.php
@@ -1,1 +1,4 @@
+<meta charset="utf-8" lang="en-IN" />
+<meta name="description" content="Web and Coding Club, IIT Bombay's Security Capture the Flag Contest" />
+<meta name = "author" content="cheekujodhpur" />
 <script src = "js/jquery-2.1.4.min.js"></script>
