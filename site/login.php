5f3e7b64424aacf3a53b794c8a920f07e6ec4108
Kumar Ayush <cheekujodhpur@gmail.com>
2015-12-30 21:56:16 -0500

    limit on user

--- site/login.php
+++ site/login.php
@@ -16,9 +16,9 @@
 <h3>Register</h3>
 <form action="register.php" method="POST">
     <label for="username">Username:</label>
-    <input type = "text" name = "username" />
+    <input type = "text" name = "username" /> &nbsp; (5-15 characters)
     <br />
-    <label for="password">Password:</label>
+    <label for="password">Password:</label> &nbsp;  (8-15 characters)
     <input type = "password" name = "password" />
     <br />
     <label for="repassword">Re-enter Password:</label>
